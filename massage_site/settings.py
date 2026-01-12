de6248d456dc634d1d238f70c0eb927d17e37be4
chadrackmbupiokajess <chadrackmbupiokajess@gmail.com>
2026-01-12 13:08:57 +0100

    ajout email

--- massage_site/settings.py
+++ massage_site/settings.py
@@ -23,10 +23,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY', default='django-insecure-!your-secret-key-here!')
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', default=True, cast=bool)
+SECRET_KEY = config('SECRET_KEY')
+DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = ['relaxrestoremassage.pythonanywhere.com','127.0.0.1']
 
